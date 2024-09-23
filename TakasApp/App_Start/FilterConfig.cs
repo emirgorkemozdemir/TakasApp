3614d6a0496dc5be1fadda0afeb01d15041e07e6
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2024-09-23 15:29:40 +0300

    Add project files.

---
+++ TakasApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TakasApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TakasApp/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TakasApp.App_Start;
+using TakasApp.Models;
+
+namespace TakasApp.Controllers
+{
+    public class UserController : Controller
+    {
+        TakasDBEntities db = new TakasDBEntities();
+
+        [HttpGet]
+        public ActionResult UserAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserAdd(TableUser registered_user)
+        {
+            if (ModelState.IsValid)
+            {
+                registered_user.UserRegisterDate=DateTime.Now.Date;
+                registered_user.UserPassword = Sha256Compute.ComputeSha256Hash(registered_user.UserPassword);
+                db.TableUser.Add(registered_user);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(TableUser selected_user)
+        {
+            string hashed_password = Sha256Compute.ComputeSha256Hash(selected_user.UserPassword);
+            var logged_user= db.TableUser.Where(u => u.UserName == selected_user.UserName && u.UserPassword == hashed_password).SingleOrDefault();
+
+            if (logged_user==null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return RedirectToAction("MainPage");
+            }   
+          
+        }
+
+        // Register yapan ekip : şifre kayıt olurken sha256 kullanılarak şifrelenip gönderilmeli
+        // Login : Gelen şifre sha256 oldugu için, girilen şifreyi de dönüştürüp kontrol etmeniz lazm.
+    }
+}
